9ed323eb8d4889d1ad19e7cfe2aa29b2dc41f5c1
lightt77 <abhisheksatam17@gmail.com>
2018-10-07 22:39:35 +0530

    Setup router for navigation

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes} from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ProductComponent } from './components/product/product.component';
+
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'product', component:ProductComponent}
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
