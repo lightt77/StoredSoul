dc01406ab4631a0544537cbe0a52b415256d1624
lightt77 <abhisheksatam17@gmail.com>
2018-10-13 16:22:20 +0530

    Add image field for product and user

--- sqlscripts/v1/CreateDatabaseAndTables.sql
+++ sqlscripts/v1/CreateDatabaseAndTables.sql
@@ -3,13 +3,14 @@ create database StoredSoul;
 use StoredSoul;
 
 create table user(
-    user_id INT PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
-    email_id VARCHAR(30) NOT NULL UNIQUE,
-    password VARCHAR(30) NOT NULL
+    user_id int primary key not null unique auto_increment,
+    email_id varchar(30) not null unique,
+    password varchar(30) not null,
+    image_path varchar(100) unique
 );
 
 create table product(
-	product_id INT not null auto_increment primary key,
+	product_id int not null auto_increment primary key,
     name varchar(30) not null unique,
     type enum('tshirt', 'tshirt-dress', 'mobile-cover', 'backpack', 'dufflebag', 'boxer', 'badge', 'mug', 'magnet', 'socks', 'cap', 'flopflops', 'notebook', 'poster', 'sticker', 'tote-bag', 'hoodie', 'action-figure', 'keychain', 'others') default 'others',
 	colour varchar(20) not null,
@@ -18,11 +19,12 @@ create table product(
     artist_id int not null,
     date_added datetime default now(),
     no_of_purchases int default 0,
+    image_path varchar(100) unique,
     foreign key(artist_id) references user(user_id)
 );
 
 create table product_instance(
-	product_instance_id INT not null auto_increment primary key,
+	product_instance_id int not null auto_increment primary key,
     product_id int not null,
     purchaser_id int default -1,	# -1 here means available
     foreign key(product_id) references product(product_id),
@@ -30,7 +32,7 @@ create table product_instance(
 );
 
 create table tag(
-	tag_id INT not null auto_increment primary key,
+	tag_id int not null auto_increment primary key,
     tag_name varchar(20) not null unique
 );
 
