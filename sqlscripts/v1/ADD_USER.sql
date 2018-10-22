2c96f6319a044acd998a4b44c74f12a02002bd67
lightt77 <abhisheksatam17@gmail.com>
2018-10-23 01:29:21 +0530

    Refactor existing SPs

--- sqlscripts/v1/ADD_USER.sql
+++ sqlscripts/v1/ADD_USER.sql
@@ -1,6 +1,4 @@
-create procedure ADD_USER(in email_id varchar(255), in password varchar(255))
+create procedure ADD_USER(in email_id varchar(255), in password varchar(255), in image_path varchar(255))
 begin
-	insert into User(email_id, password) values(email_id, password);
+	insert into User(email_id, password, image_path) values(email_id, password, image_path);
 end
-
-call ADD_USER('shikamaru@ss.com', 'shikamaru');
