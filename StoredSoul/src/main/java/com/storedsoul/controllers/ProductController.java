396e0d03e7f72bf3b10003359639ed379e389c8a
lightt77 <abhisheksatam17@gmail.com>
2018-11-11 17:19:54 +0530

    Add temporary cross-origin permission on getAvailableProducts endpoint

--- StoredSoul/src/main/java/com/storedsoul/controllers/ProductController.java
+++ StoredSoul/src/main/java/com/storedsoul/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.storedsoul.controllers;
 import com.storedsoul.models.Product;
 import com.storedsoul.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,8 @@ public class ProductController
     @Autowired
     private ProductService productService;
 
+    // TODO: remove cross origin beforedeploying
+    @CrossOrigin(origins = "*")             // only for debugging
     @GetMapping
     public List<Product> getAvailableProducts()
     {
